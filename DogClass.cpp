a2c72c6f49e46954197126c61df8c90a54c7b760
Clair Merry <cmerry@atu.edu>
2026-01-21 23:03:38 -0600

    Set up class and private data members om dog class

--- DogClass.cpp
+++ DogClass.cpp
@@ -1,10 +1,29 @@
 //Class Activity 1: Github
 
+#include <iostream>
+#include <string>
+
 using namespace std;
 
-#include <iostream>
+//dog class
+class Dog
+{
+//private data members
+private:
+	string name, breed;
+	int age;
+
+//member functions
+public:
+
+};
 
 int main()
 {
+	Dog dog1;
 
+
+
+
+	return 0;
 }
